1ed24106f1ce7db359dbb6b6d10f74bf9dc1d130
ganbari <steinway.nathan@gmail.com>
2021-12-18 19:42:15 -0600

    enabled site to respond to headers

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,9 @@ var getRepoIssues = function(repo) {
             });
         } else {
             alert("there was a problem with your request!");
+        }
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
         }
     });
 
@@ -43,5 +47,20 @@ var displayIssues = function (issues) {
 
     }
 }
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+
+  
 
 getRepoIssues("facebook/react");
