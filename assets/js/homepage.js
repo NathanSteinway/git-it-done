28c0176fd77df005670a29fd9171af9af6f6be25
ganbari <steinway.nathan@gmail.com>
2021-12-18 20:09:11 -0600

    added error handling features

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -53,21 +53,25 @@ var displayRepos = function(repos, searchTerm) {
     for (var i = 0; i < repos.length; i++) {
 
         var repoName = repos[i].owner.login + "/" + repos[i].name;
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var repoEl = document.createElement("a");
+            repoEl.classList = "list-item flex-row justify-space-between align-center";
+            repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+            
         var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
-        repoEl.appendChild(titleEl);
+            titleEl.textContent = repoName;
+            repoEl.appendChild(titleEl);
 
         var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center";
+            statusEl.classList = "flex-row align-center";
 
-        if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML =
-                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-        } else {
-            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-        }
+            if (repos[i].open_issues_count > 0) {
+                statusEl.innerHTML =
+                    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+            } else {
+                statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+            }
+
         repoContainerEl.appendChild(statusEl);
         repoContainerEl.appendChild(repoEl);
     }
