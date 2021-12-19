3e5e9d9ab73032c81ab987c270e59f1265be0fa9
ganbari <steinway.nathan@gmail.com>
2021-12-18 20:13:03 -0600

    bug fix

--- assets/js/single.js
+++ assets/js/single.js
@@ -19,6 +19,9 @@ var getRepoName = function() {
   };
 
 var getRepoIssues = function(repo) {
+
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
